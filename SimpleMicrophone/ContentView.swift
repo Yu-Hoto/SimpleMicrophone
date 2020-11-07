87b398a1a269a4f7cc010123d2e91f71aab2d2d1
Yu-Hoto <yu.hoto.0220@gmail.com>
2020-11-07 23:21:23 +0900

    v 0.0.1

--- SimpleMicrophone/ContentView.swift
+++ SimpleMicrophone/ContentView.swift
@@ -6,11 +6,54 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    private var engine = AVAudioEngine()
+    
+    @State private var powerIsOn = true
+    @State private var volume: Float = 0.3
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Toggle("Power Switch", isOn: $powerIsOn)
+            .onChange(of: powerIsOn, perform: { value in
+                toggleEngine(powerIsOn)
+            })
+            .toggleStyle(SwitchToggleStyle())
+            
+            Slider(value: $volume, in: 0...1, onEditingChanged: { _ in
+                engine.inputNode.volume = volume
+            })
+        }.padding()
+        .onAppear() {
+            setupAudioSession()
+            let input = engine.inputNode
+            let output = engine.outputNode
+            let format = engine.inputNode.inputFormat(forBus: 0)
+                
+            engine.connect(input, to: output, format: format)
+            engine.inputNode.volume = volume
+            try! engine.start()
+        }
+    }
+
+    func toggleEngine(_ power: Bool) {
+        if power {
+            try? engine.start()
+        } else {
+            try? engine.stop()
+        }
+    }
+
+    func setupAudioSession() {
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(.playAndRecord, options: [.allowBluetoothA2DP])
+            try session.setActive(true)
+        } catch {
+            fatalError("Failed to configure and activate session.")
+        }
     }
 }
 
