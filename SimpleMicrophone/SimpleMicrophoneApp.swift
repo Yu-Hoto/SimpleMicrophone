a082c08a16e79a057c74c94fd514720271946b9f
Yu-Hoto <yu.hoto.0220@gmail.com>
2020-11-07 22:55:05 +0900

    Initial Commit

---
+++ SimpleMicrophone/SimpleMicrophoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMicrophoneApp.swift
+//  SimpleMicrophone
+//
+//  Created by Yu Hoto on 2020/11/07.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMicrophoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
